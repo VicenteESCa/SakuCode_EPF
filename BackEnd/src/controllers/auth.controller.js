b497b09b5669fa53ebc44808314075a2b5e1104a
Vicente Escarate <vicente.escarate.l@mail.pucv.cl>
2024-11-15 13:33:46 -0300

    Ojito que instale dep nuevas, asique hae el npm install

--- BackEnd/src/controllers/auth.controller.js
+++ BackEnd/src/controllers/auth.controller.js
@@ -1,16 +1,31 @@
 import User from "../models/user.model.js"
-
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
 export const register = async (req, res) => {
+    /**Lectura del body en la peticion */
     console.log("body recibido", req.body)
     const {username,email,password} = req.body
+    /**Try catch para la asincronia */
     try{
+
+        const hashs = await bcrypt.hash(password,10) 
+
         const newUser = new User({
             username,
             email,
-            password
+            password:hashs,
         })
         const usuarioSaved = await newUser.save()
-        res.json(usuarioSaved)
+        
+
+        // Solo devuelve un valor definido con el post
+        res.json({
+            id: usuarioSaved._id,
+            username: usuarioSaved.username,
+            email: usuarioSaved.,
+            createdAt: usuarioSaved.createdAt,
+            updatedAt: usuarioSaved.updatedAt,
+        })
     }catch(error){
         console.error("Error al registrar usuario:", error);
         res.status(500).send("Error al registrar usuario");
@@ -33,4 +48,4 @@ export const login = async (req,res) => {
         console.error("Error de acceder al ususario", error)
         res.status(500).send("Error de inicio de sesion")
     }
-} 
+}   
