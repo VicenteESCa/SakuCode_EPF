6cd040945e1fb2363f4d7f3cb168bcfc7af2ee67
Vicente Escarate <vicente.escarate.l@mail.pucv.cl>
2024-11-15 13:15:41 -0300

    Creacion de las bases para el registro de usuario

--- BackEnd/src/controllers/auth.controller.js
+++ BackEnd/src/controllers/auth.controller.js
@@ -3,22 +3,34 @@ import User from "../models/user.model.js"
 export const register = async (req, res) => {
     console.log("body recibido", req.body)
     const {username,email,password} = req.body
-    
     try{
         const newUser = new User({
             username,
             email,
             password
         })
-        await newUser.save()
-        res.send("registrando")
+        const usuarioSaved = await newUser.save()
+        res.json(usuarioSaved)
     }catch(error){
         console.error("Error al registrar usuario:", error);
         res.status(500).send("Error al registrar usuario");
     }
 }
 
-export const login = (req,res) => {
-    console.log(req.body) 
-    res.send('logeado')
+export const login = async (req,res) => {
+    console.log("Body recibido", req.body)
+    const {usernmae, email, password} = req.body
+    try{
+        const user = User({
+            username,
+            email,
+            password
+        })
+        await user.res()
+        res.send("Inicio de sesion")
+        
+    }catch(error){
+        console.error("Error de acceder al ususario", error)
+        res.status(500).send("Error de inicio de sesion")
+    }
 } 
--- BackEnd/src/models/user.model.js
+++ BackEnd/src/models/user.model.js
@@ -16,6 +16,10 @@ const userSchema = new mongoose.Schema({
         type:String,
         required: true,
     }
+}, {
+    timestamps: true,
+    collection: "users",
+    autoCreate: true
 })  
 /**
  * Pluraliza el nombre para crear una coleccion de usuarios 
